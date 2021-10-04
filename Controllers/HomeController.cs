bd3ba6585d3f2351b3dbd2dcbd19fd1f526f0d1c
Doug Herman <douglash9882@gmail.com>
2021-10-04 14:57:43 -0500

    fixes

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,10 +19,31 @@ namespace RandomPasscode.Controllers
             _logger = logger;
         }
 
+        [HttpGet ("")]
         public IActionResult Index()
         {
+            if (HttpContext.Session.GetInt32("count") == null)
+            {
+                HttpContext.Session.SetInt32("count", 0);
+            }
+            ViewBag.Count = HttpContext.Session.GetInt32("count");
             return View();
         }
+        
+
+        [HttpGet ("click")]
+        public IActionResult GenNew()
+        {
+            int? count = HttpContext.Session.GetInt32("count");
+            count++;
+            HttpContext.Session.SetInt32("count", (int)count);
+            return RedirectToAction("Index");
+        }
+
+
+// ================= Presets ===============================================
+
+
 
         public IActionResult Privacy()
         {
